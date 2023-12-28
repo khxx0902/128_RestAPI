629c96523193d2ae036a03f0da2ba1fc42aa41c6
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-12-28 21:32:59 +0700

    add class kontak ap[liaction

---
+++ app/src/main/java/com/example/restapi/ui/KontakApplication.kt
@@ -0,0 +1,14 @@
+package com.example.restapi.ui
+
+import android.app.Application
+import com.example.restapi.repository.AppContainer
+import com.example.restapi.repository.KontakContainer
+
+class KontakApplication : Application(){
+    lateinit var container: AppContainer
+
+    override fun onCreate(){
+        super.onCreate()
+        container = KontakContainer()
+    }
+}
