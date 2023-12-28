8543f1c7562e37774f266bf267a189c0c0633cd4
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-12-28 22:27:15 +0700

    add data class InsertUiState

---
+++ app/src/main/java/com/example/restapi/ui/InsertViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.restapi.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.restapi.repository.KontakRepositori
+import kotlinx.coroutines.launch
+
+class InsertViewModel (private val kontakRepository: KontakRepositori) : ViewModel(){
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak(){
+        viewModelScope.launch {
+            try{
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+)
