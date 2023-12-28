ff40cbc05c59d0cef36868b56da87b9ead73cc81
mkhaidar168@gmail.com <mkhaidar168@gmail.com>
2023-12-28 22:17:31 +0700

    add object destinasinavigasi in InsertScreen.kt

---
+++ app/src/main/java/com/example/restapi/ui/InsertScreen.kt
@@ -0,0 +1,8 @@
+package com.example.restapi.ui
+
+import com.example.restapi.navigasi.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Siswa"
+}
